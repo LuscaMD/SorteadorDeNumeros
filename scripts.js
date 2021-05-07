a81614993f36b6a9c152577011a2edb2b27d72e6
Lucas Martins <70406462+LuscaMD@users.noreply.github.com>
2021-05-07 08:35:21 -0300

    Sorteando numeros negativos / CSS básico

--- scripts.js
+++ scripts.js
@@ -2,10 +2,22 @@ function gerarNumeroAleatorio(numMinimo, numMaximo){
 
     var numMinimo = parseInt(document.getElementById("iptMinimo").value);
     var numMaximo = parseInt(document.getElementById("iptMaximo").value);    
-    var numeroGerado = Math.floor((Math.random() * numMaximo) + numMinimo);
 
-    while (numeroGerado > numMaximo){
-        numeroGerado = Math.floor((Math.random() * numMaximo) + numMinimo);
+    if(numMinimo < 0){
+
+        var numeroGerado = Math.floor((Math.random() * (numMaximo - (2 * numMinimo)) + numMinimo));
+
+        while (numeroGerado > numMaximo){                             
+            numeroGerado = Math.floor((Math.random() * (numMaximo - (2 * numMinimo)) + numMinimo));
+        }
+    }
+    else{
+
+        var numeroGerado = Math.floor((Math.random() * ((numMaximo + 1) - numMinimo)) + numMinimo);
+    
+        while (numeroGerado > numMaximo){                             
+            numeroGerado = Math.floor((Math.random() * ((numMaximo + 1) - numMinimo)) + numMinimo);
+        }
     }
 
     document.getElementById("numeroGerado").innerText = numeroGerado;
