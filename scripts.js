ec39d844f02ce5a113cc814e892d59f22caffac6
Lucas Martins <70406462+LuscaMD@users.noreply.github.com>
2021-05-08 10:53:13 -0300

    Escolhendo quantas vezes deseja sortear / ReadMe

--- scripts.js
+++ scripts.js
@@ -1,24 +1,37 @@
-function gerarNumeroAleatorio(numMinimo, numMaximo){
+function gerarNumeroAleatorio(numVezes,numMinimo, numMaximo, resultado){
 
+    var numVezes = parseInt(document.getElementById("iptVezes").value);
     var numMinimo = parseInt(document.getElementById("iptMinimo").value);
-    var numMaximo = parseInt(document.getElementById("iptMaximo").value);    
+    var numMaximo = parseInt(document.getElementById("iptMaximo").value); 
+    var resultado, i = 0;
 
-    if(numMinimo < 0){
+    do{
 
-        var numeroGerado = Math.floor((Math.random() * (numMaximo - (2 * numMinimo)) + numMinimo));
-
-        while (numeroGerado > numMaximo){                             
-            numeroGerado = Math.floor((Math.random() * (numMaximo - (2 * numMinimo)) + numMinimo));
-        }
-    }
-    else{
-
-        var numeroGerado = Math.floor((Math.random() * ((numMaximo + 1) - numMinimo)) + numMinimo);
+        if(numMinimo < 0){
+            var numeroGerado = Math.floor((Math.random() * (numMaximo - (2 * numMinimo)) + numMinimo));
     
-        while (numeroGerado > numMaximo){                             
-            numeroGerado = Math.floor((Math.random() * ((numMaximo + 1) - numMinimo)) + numMinimo);
+            while (numeroGerado > numMaximo){                             
+                numeroGerado = Math.floor((Math.random() * (numMaximo - (2 * numMinimo)) + numMinimo));
+            }
         }
-    }
+        else{
+            var numeroGerado = Math.floor((Math.random() * ((numMaximo + 1) - numMinimo)) + numMinimo);
+        
+            while (numeroGerado > numMaximo){                             
+                numeroGerado = Math.floor((Math.random() * ((numMaximo + 1) - numMinimo)) + numMinimo);
+            }
+        }
 
-    document.getElementById("numeroGerado").innerText = numeroGerado;
+        console.log(numeroGerado);
+        if(i==0){
+            resultado = numeroGerado;
+        }
+        else{
+            resultado = resultado +", " +numeroGerado;
+        }
+        
+        i++;
+    }while(i<numVezes);
+    
+    document.getElementById("numeroGerado").innerText = resultado;
 }
